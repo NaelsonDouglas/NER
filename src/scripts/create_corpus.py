b7e2b924e274716044632c3e37bd30f437dd957f
naelsondouglas <naelson17@gmail.com>
2022-09-18 16:59:03 -0300

    trainer finished

--- src/scripts/create_corpus.py
+++ src/scripts/create_corpus.py
@@ -1,5 +1,6 @@
 import json
 import random
+import re
 
 import pandas as pd
 import spacy
@@ -18,7 +19,9 @@ class TagMaker:
         patterns = dict()
         data = []
         for _, row in tqdm(list(df.iterrows())):
-            title = row[TITLE]
+            title = row[TITLE].strip()
+            title = re.sub(r'-|\n|\t', ' ', title)
+            title = re.sub(r'\s{2,}', ' ', title)
             if title:
                 all_ners = []
                 for tag in ner_tags:
@@ -30,18 +33,21 @@ class TagMaker:
                     train_cell = [title, all_ners]
                     data.append(train_cell)
         train, test_eval = train_test_split(data, test_size = 0.8)
-        test, validation = train_test_split(data, test_size = 0.5)
+        test, dev = train_test_split(data, test_size = 0.5)
+        train, test, dev = self.persist(train, test, dev)
+        return train, test, dev
 
-        self.create_docbin(train, 'train')
-        self.create_docbin(test, 'test')
-        self.create_docbin(validation, 'validation')
+    def persist(self, train, test, dev) -> tuple:
         with open('corpus/train.json', 'w') as train_io,\
             open('corpus/test.json', 'w') as test_io,\
             open('corpus/validation.json', 'w') as validation_io:
             train_io.write(json.dumps(train))
             test_io.write(json.dumps(test))
-            validation_io.write(json.dumps(validation))
-        return train, test, validation
+            validation_io.write(json.dumps(dev))
+        train = self.create_docbin(train, 'train')
+        test = self.create_docbin(test, 'test')
+        dev = self.create_docbin(dev, 'dev')
+        return (train, test, dev)
 
     def create_docbin(self, tagged_text:list, output_basename:str) -> DocBin:
         """
@@ -58,9 +64,9 @@ class TagMaker:
                 span = doc.char_span(start, end, label=tag)
                 if span:
                     spans_list.append(span)
-                doc.set_ents(spans_list)
-                doc.spans[tag] = list(doc.ents)
-                doc_bin.add(doc)
+                    doc.set_ents(spans_list)
+                    doc.spans['sc'] = list(doc.ents)
+                    doc_bin.add(doc)
         doc_bin.to_disk(f'corpus/{output_basename}.spacy')
         return doc_bin
 
@@ -93,7 +99,9 @@ if __name__ == '__main__':
         df = pd.read_csv('assets/ds_13k.csv',on_bad_lines='warn')
     except FileNotFoundError:
         df = pd.read_csv('ds_13k.csv',on_bad_lines='warn')
-    df = df[KEEP_COLUMNS].dropna().sample(30)
+    df = df[KEEP_COLUMNS].dropna().sample(200)
     tag_maker = TagMaker()
-    TRAIN_DATA, TEST_DATA, VALIDATION_DATA = tag_maker.build_tags(df, FEATURES)
+    train, test, val = tag_maker.build_tags(df, FEATURES)
+    # train = list(train.get_docs(nlp.vocab))
+
 
