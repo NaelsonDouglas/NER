39fb147943fe238a5eb8aae210a7c77b2aca2414
naelsondouglas <naelson17@gmail.com>
2022-09-24 14:33:04 -0300

    updated the playground

--- src/playground.py
+++ src/playground.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import spacy
-from scripts.constants import KEEP_COLUMNS, TITLE, MODELNOQ
+from scripts.constants import KEEP_COLUMNS, TITLE, MODELNOQ, FEATURES
 
 
 def apply(sample:pd.DataFrame, model, lowercase:bool=False) -> None:
@@ -21,3 +21,20 @@ if __name__ == '__main__':
     df = df.query('~title.isnull()')
     df = df.fillna('')
     df = df.groupby([TITLE,MODELNOQ]).first().reset_index(drop=False)
+    sample = df.sample(1500)
+    sample['doc'] = sample.title.apply(model)
+    sample['span'] = sample.doc.apply(lambda x: dict([(span.label_,str(span)) for span in x.spans['sc'] if str(span)]))
+    sample = sample.drop(columns=['doc'])
+    print('-------\n')
+    for _, row in sample.iterrows():
+        print('========= TITLE ==============')
+        print(row.title)
+        print()
+        print('========= ANOTATIONS =========')
+        print(row[FEATURES])
+        print()
+        print('========= DETECTED ===========')
+        print(row.span)
+        print('=============================')
+        print()
+        print('-------\n')
